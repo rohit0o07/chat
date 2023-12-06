015a29b60b780b86667b7834c30e855a3b067083
rohitOo007 <hamilpur003@gmail.com>
2023-12-06 20:30:11 +0530

    add amiddleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -23,4 +23,15 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user",userRoutes);
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
+}
+)
