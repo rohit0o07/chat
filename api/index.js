46fcd32bde3ea49c40e66ae1f2925089fe7b8b83
rohitOo007 <hamilpur003@gmail.com>
2023-12-08 19:00:05 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -22,16 +22,15 @@ app.listen(3000, () => {
     console.log('Server listening on port 3000!');
 });
 
-app.use("/api/user",userRoutes);
-app.use("/api/auth",authRoutes);
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || 'internal Server Error';
+    const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
         success:false,
         message,
         statusCode,
-    })
-}
-)
+    });
+});
